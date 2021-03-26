fbeabad7930dc5c62fdba517590781cdd8966868
cpach96 <cpach96@gmail.com>
2021-03-25 22:10:53 -0400

    commit tree 564bdcdbc1631484eb82efca42159f76b97d6214

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(sourceArray,sourceFunction){
+    let newArr = []
+    for (let i = 0; i < sourceArray.length; i++){
+        newArr.push(sourceFunction(sourceArray[i]))
+    }
+    return newArr
+}
+
+function reduce(sourceArray,sourceFunction,startingPoint){
+    if(startingPoint){
+      return sourceArray.reduce(sourceFunction,startingPoint)
+    }else if (sourceArray.reduce(sourceFunction) === true){
+        return true
+    } else {
+        return sourceArray.reduce(sourceFunction,0)
+    }
+    
+}
